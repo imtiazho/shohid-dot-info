1a3b38ce32020f55fc801c628540180d1835e2f2
Imtiaz <axubair9@gmail.com>
2024-08-03 03:25:35 +0600

    Inital file set up with Bhai karo pani lagbe

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className='text-3xl mt-10'>ভাই! কারো পানি লাগবে...</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FFA500",
+          secondary: "#F2F2F2",
+          accent: "#1FB2A5",
+          neutral: "#313131",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+}
+
